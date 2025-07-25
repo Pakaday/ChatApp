90463f3947bf4f8188ea448a8351ee1285fbf604
Frederick Neimeister <pakaday@me.com>
2025-07-25 10:15:38 -0400

    Initial commit

---
+++ Data/ChatDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ChatApp.Models;
+
+namespace ChatApp.Data
+{
+	public class ChatDbContext : DbContext
+	{
+		public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<User> Users { get; set; } = null!;
+		public DbSet<Message> Messages { get; set; } = null!;
+	}
+}
---
+++ Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatApp.Models
+{
+	public class Message
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Content { get; set; } = string.Empty;
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public User? User { get; set; } // Navigation property to the User who sent the message
+		public bool IsDeleted { get; set; } = false;
+	}
+}
