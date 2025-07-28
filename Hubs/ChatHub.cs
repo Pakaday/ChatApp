7c433d8dc13836235dc931ade2f0ea4ec64fa88d
Frederick Neimeister <pakaday@me.com>
2025-07-28 12:19:07 -0400

    Add save messages functionality to database

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using ChatApp.Data;
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Models;
 
 namespace ChatApp.Hubs
 {
 	public class ChatHub : Hub
 	{
+
+		private readonly ChatDbContext _context;
+
+		public ChatHub(ChatDbContext context)
+		{
+			_context = context;
+		}
 		public async Task SendMessage(string user, string message)
 		{
+			var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user);
+			if (dbUser == null) return;
+
+			var newMessage = new Message
+			{
+				UserId = dbUser.Id,
+				Content = message,
+				CreatedAt = DateTime.UtcNow,
+				IsDeleted = false
+			};
+
+			_context.Messages.Add(newMessage);
+			await _context.SaveChangesAsync();
+
 			await Clients.All.SendAsync("ReceiveMessage", user, message);
 		}
 	}
