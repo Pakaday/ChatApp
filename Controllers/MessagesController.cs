5117d59e13f6794bbf0725a3dbacee52cefd1cbd
Frederick Neimeister <pakaday@me.com>
2025-07-29 12:24:46 -0400

    MessagesController made for API calls

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Data;
+using ChatApp.Models;
+
+namespace ChatApp.Controllers
+{
+	[ApiController]
+	[Route("api/messages")]
+	public class MessagesController : ControllerBase
+	{
+		private readonly ChatDbContext _context;
+
+		public MessagesController(ChatDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetMessages()
+		{
+			var messages = await _context.Messages
+				.Include(m => m.User)
+				.OrderBy(m => m.CreatedAt)
+				.ToListAsync();
+
+			var result = messages.Select(m => new
+			{
+				user = m.User.Username,
+				message = m.Content,
+				createdAt = m.CreatedAt
+			});
+
+			return Ok(result);
+		}
+	}
+}
